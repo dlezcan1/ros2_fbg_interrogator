5ff17ce4d26871183b461197e12882a0f09c4c44
Dimitri Lezcano <dalezcano19@gmail.com>
2022-03-29 10:17:05 -0400

    Added python package

---
+++ sm130_interrogator_py/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'sm130_interrogator_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dimitri Lezcano',
+    maintainer_email='dlezcan1@jhu.edu',
+    description='Micron Optics sm130 Interrogatory python package',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sm130_interrogator = sm130_interrogator_py.sm130_interrogator:main'
+        ],
+    },
+)
---
+++ sm130_interrogator_py/sm130_interrogator_py/sm130_interrogator.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from sm130_interrogator_py.')
+
+
+if __name__ == '__main__':
+    main()
